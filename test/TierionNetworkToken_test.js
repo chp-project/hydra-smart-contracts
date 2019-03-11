b1ed88d152c86c31b3934bbf462238a747b1c6b7
Michael Iglesias <miglesias@tierion.com>
2019-03-11 12:18:00 -0700

    Applied unit tests for token from stash just for sake of completeness, everything tested in the unit tests are also tested in the e2e test-suite

---
+++ test/TierionNetworkToken_test.js
@@ -0,0 +1,48 @@
+const TierionNetworkToken = artifacts.require("TierionNetworkToken");
+const ChainpointRegistry = artifacts.require("ChainpointRegistry");
+const ethers = require('ethers');
+const web3 = require('web3');
+
+contract("TierionNetworkToken", async (accounts) => {
+  it("should put 100000000000000000 Grains in the first account", async () => {
+    let tokenContract = await TierionNetworkToken.deployed();
+    let balance = await tokenContract.balanceOf.call(accounts[0]);
+
+    assert.equal(balance.valueOf(), 100000000000000000);
+  });
+
+  it("should return 100000000000000000 Grains as totalSupply", async () => {
+    let tokenContract = await TierionNetworkToken.deployed();
+    let balance = await tokenContract.totalSupply.call();
+
+    assert.equal(balance.valueOf(), 100000000000000000);
+  });
+
+  it("should transfer 500000000000 Grains from Account #0 -> Account #1", async () => {
+    let tokenContract = await TierionNetworkToken.deployed();
+    await tokenContract.transfer(accounts[1], 500000000000, { from: accounts[0] })
+    let balance = await tokenContract.balanceOf.call(accounts[1]);
+    assert.equal(balance.valueOf(), 500000000000);
+  });
+
+  it("should grant allowance of 500000000000 Grains to Account #0 on behalf of Account #1", async () => {
+    let tokenContract = await TierionNetworkToken.deployed();
+    let registryContract = await ChainpointRegistry.deployed();
+
+    await tokenContract.approve(accounts[0], 500000000000, { from: accounts[1] })
+    let allowance = await tokenContract.allowance(accounts[1], accounts[0]);
+
+    assert.equal(allowance.valueOf(), 500000000000);
+  });
+
+  it("should transfer 500000000000 grains from Account #1 -> Account #0 after allowance has been granted", async () => {
+    let tokenContract = await TierionNetworkToken.deployed();
+    let registryContract = await ChainpointRegistry.deployed();
+
+    await tokenContract.transferFrom(accounts[1], accounts[0], 500000000000, { from: accounts[0] })
+    let balance = await tokenContract.balanceOf.call(accounts[0]);
+
+    assert.equal(balance.valueOf(), 100000000000000000);
+  });
+
+});
