33bd3c446b823aa6b8d683450a184d00d4b6c69e
Michael Iglesias <miglesias@tierion.com>
2019-03-12 21:11:57 -0700

    .

--- migrations/2_erc20_registry_migration.js
+++ migrations/2_erc20_registry_migration.js
@@ -7,13 +7,13 @@ var ChainpointQuorum = artifacts.require("ChainpointQuorum");
 module.exports = function(deployer, network) {
   // deployment steps
   deployer.deploy(TierionNetworkToken).then(function() {
-    fs.writeFileSync(path.resolve(__dirname, 'e2e-testing/artifacts/ropsten_token.txt'), TierionNetworkToken.address, 'utf8');
+    fs.writeFileSync(path.resolve('./e2e-testing/artifacts/ropsten_token.txt'), TierionNetworkToken.address, 'utf8');
     return deployer.deploy(ChainpointRegistry, TierionNetworkToken.address);
   }).then(function() {
-    fs.writeFileSync(path.resolve(__dirname, 'e2e-testing/artifacts/ropsten_registry.txt'), ChainpointRegistry.address, 'utf8');
+    fs.writeFileSync(path.resolve('./e2e-testing/artifacts/ropsten_registry.txt'), ChainpointRegistry.address, 'utf8');
     return deployer.deploy(ChainpointQuorum, TierionNetworkToken.address, ChainpointRegistry.address);
   }).then(() => {
-    fs.writeFileSync(path.resolve(__dirname, 'e2e-testing/artifacts/ropsten_quorum.txt'), ChainpointQuorum.address, 'utf8');
+    fs.writeFileSync(path.resolve('./e2e-testing/artifacts/ropsten_quorum.txt'), ChainpointQuorum.address, 'utf8');
     return;
   })
 };
