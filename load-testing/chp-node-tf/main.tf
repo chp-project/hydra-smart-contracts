6eb55ddffca50f6ddae0016ac38027e429b039b1
Michael Iglesias <miglesias@tierion.com>
2019-07-19 14:07:04 -0700

    Triggering Chainpoint Node Mass Provisioner : 100 NODE(s)

--- load-testing/chp-node-tf/main.tf
+++ load-testing/chp-node-tf/main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 resource "google_compute_instance" "chp-node" {
-  name         = "chp-node-r2-${count.index}"
+  name         = "chp-node-r3-${count.index}"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
   count        = "${var.node_count}"
--- load-testing/chp-node-tf/variables.tf
+++ load-testing/chp-node-tf/variables.tf
@@ -1,4 +1,4 @@
 variable "node_count" {
   description = "Number of Chainpoint Nodes to provision"
-  default     = 600
+  default     = 100
 }
